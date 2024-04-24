a9e662b64161207edb2b6fb58e2554aee0565703
josedaniele <josedaniele493@gmail.com>
2024-04-24 20:22:26 -0300

    tk-01: Profiles,EditUserDTO & validaciones(regex)

---
+++ SistemaGestionGimnasioApi/SistemaGestionGimnasioApi/Data/Profiles/AdminProfile.cs
@@ -0,0 +1,22 @@
+﻿using SistemaGestionGimnasioApi.Data.Entities;
+using AutoMapper;
+using SistemaGestionGimnasioApi.Data.Models;
+
+namespace SistemaGestionGimnasioApi.Data.Profiles
+{
+    public class AdminProfile:Profile
+    {
+        public AdminProfile()
+        {
+            CreateMap<UserDto, Admin>()
+               .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+               .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName))
+               .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
+               .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password));
+
+            CreateMap<EditUserDto, Admin>()
+               .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+               .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName));
+        }
+    }
+}
