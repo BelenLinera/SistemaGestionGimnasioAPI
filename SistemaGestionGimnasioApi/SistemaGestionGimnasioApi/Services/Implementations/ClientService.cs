e1259b0950f6fc187b83beff23a7aa79ab19c957
Belen Linera <belen.linera@gmail.com>
2024-05-07 17:16:16 -0300

    TK-02 abm client

--- SistemaGestionGimnasioApi/SistemaGestionGimnasioApi/Services/Implementations/ClientService.cs
+++ SistemaGestionGimnasioApi/SistemaGestionGimnasioApi/Services/Implementations/ClientService.cs
@@ -1,6 +1,64 @@
-﻿namespace SistemaGestionGimnasioApi.Services.Implementations
+﻿using AutoMapper;
+using SistemaGestionGimnasioApi.Data.Entities;
+using SistemaGestionGimnasioApi.Data.Models;
+using SistemaGestionGimnasioApi.DBContext;
+using SistemaGestionGimnasioApi.Services.Interfaces;
+
+namespace SistemaGestionGimnasioApi.Services.Implementations
 {
-    public class ClientService
+    public class ClientService : IClientService
     {
+        private readonly SystemContext _context;
+        private readonly IMapper _mapper;
+        public ClientService(SystemContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public User GetClientByEmail(string email)
+        {
+            try
+            {
+                return _context.Clients.FirstOrDefault(u => u.Email == email);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error al obtener el cliente con Email {email}: {ex.Message}", ex);
+            }
+        }
+        public List<Client> GetAllClients()
+        {
+            return _context.Clients.Where(u => u.IsDeleted == false).ToList();
+        }
+
+        public Client CreateClient(UserDto userDto)
+        {
+            Client? userEntity = _mapper.Map<Client>(userDto);
+            _context.Clients.Add(userEntity);
+            return userEntity;
+        }
+        public Client EditClient(EditUserDto client, string emailClient)
+        {
+            Client clientToEdit = _context.Clients.SingleOrDefault(u => u.Email == emailClient);
+            if (clientToEdit == null)
+            {
+                return null;
+            }
+            Client clientEdited = _mapper.Map(client, clientToEdit);
+            _context.Clients.Update(clientEdited);
+            return clientEdited;
+        }
+        public void DeleteClient(Client clientToDelete)
+        {
+            if (clientToDelete == null)
+            {
+                throw new ArgumentNullException(nameof(clientToDelete));
+            }
+            _context.Remove(clientToDelete);
+        }
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync() > 0);
+        }
     }
 }
--- SistemaGestionGimnasioApi/SistemaGestionGimnasioApi/Services/Interfaces/IClientService.cs
+++ SistemaGestionGimnasioApi/SistemaGestionGimnasioApi/Services/Interfaces/IClientService.cs
@@ -1,6 +1,16 @@
-﻿namespace SistemaGestionGimnasioApi.Services.Interfaces
+﻿using SistemaGestionGimnasioApi.Data.Entities;
+using SistemaGestionGimnasioApi.Data.Models;
+
+namespace SistemaGestionGimnasioApi.Services.Interfaces
 {
     public interface IClientService
     {
+        public User GetClientByEmail(string email);
+        List<Client> GetAllClients();
+        Client CreateClient(UserDto userDto);
+        Client EditClient(EditUserDto clientEdited, string emailClient);
+
+        void DeleteClient(Client clientToDelete);
+        Task<bool> SaveChangesAsync();
     }
 }
