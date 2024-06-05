75971a35245951c02af7d9323dc10e46a1f9885a
josedaniele <josedaniele493@gmail.com>
2024-06-05 14:28:03 -0300

    TK-09: fix password regex

--- SistemaGestionGimnasioApi/SistemaGestionGimnasioApi/Data/Models/CreateTrainerDTO.cs
+++ SistemaGestionGimnasioApi/SistemaGestionGimnasioApi/Data/Models/CreateTrainerDTO.cs
@@ -10,10 +10,11 @@ namespace SistemaGestionGimnasioApi.Data.Models
         public string LastName { get; set; }
 
         [Required(ErrorMessage = "Este campo no puede quedar vacio")]
+        [EmailAddress(ErrorMessage = "Por favor, ingrese una dirección de correo electrónico válida.")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Este campo no puede quedar vacio")]
-        [RegularExpression("^(?=.?[A-Z])(?=.?[a-z])(?=.*?[0-9]).{8,}$", ErrorMessage = "La contraseña debe contener al menos una letra minúscula,una mayuscula, un número y al menos 8 caracteres")]
+        [RegularExpression("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$", ErrorMessage = "La contraseña debe contener al menos una letra minúscula,una mayuscula, un número y al menos 8 caracteres")]
         public string Password { get; set; }
         public List<int> Activities { get; set; } 
     }
